eae555e642fc8649ea6d12b6c8e8ad19137f3017
Vince <vinnykipx@gmail.com>
2024-02-19 19:51:57 +0300

    analytics tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,18 @@
   import "./styles.css";
   import MobileMenu from "$components/MobileMenu/MobileMenu.svelte";
 
+
   let bgStyles = "";
+ 
+</script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-1466RY5B8K"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-1466RY5B8K');
 </script>
 
 <div class="app">
